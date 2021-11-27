f2c64a16ee5e086cf0208af107bd0fdc1ae53930
Dmytro <dimasoltusyuk@gmail.com>
2021-11-27 17:52:29 +0200

    feat: editor component and useCodeMirror hook

--- live_text-web/components/Editor.tsx
+++ live_text-web/components/Editor.tsx
@@ -1,6 +1,28 @@
-export const Editor: React.FC<{
+import { useCallback, useEffect } from 'react'
+import { useCodeMirror, UseCodeMirrorProps } from '../hooks/useCodeMirror'
+import styles from '../styles/Editor.module.scss'
+
+interface Props {
     initialDoc: string
     onChange: (doc: string) => void
-}> = () => {
-    return <span>hello world</span>
+}
+
+export const Editor: React.FC<Props> = ({ initialDoc, onChange }) => {
+    // Special callback for codemirror
+    const handleChange = useCallback<
+        NonNullable<UseCodeMirrorProps['onChange']>
+    >(({ doc }) => onChange(doc.toString()), [onChange])
+
+    const [refContainer, editorView] = useCodeMirror<HTMLDivElement>({
+        initialDoc,
+        onChange: handleChange,
+    })
+
+    useEffect(() => {
+        if (editorView) {
+            // TODO: create a note
+        }
+    }, [editorView])
+
+    return <div className={styles.editorWrapper}></div>
 }
